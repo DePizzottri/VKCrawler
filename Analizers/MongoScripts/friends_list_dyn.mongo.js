b9bf4c511539321eba4519557dd8499c3a2fa524
DePizzottri <depizzottri@gmail.com>
2015-03-20 18:08:54 +0800

    Common and analitic scripts

--- Analizers/MongoScripts/friends_list_dyn.mongo.js
+++ Analizers/MongoScripts/friends_list_dyn.mongo.js
@@ -1,11 +1,62 @@
-//most friended
-db.friends_list.aggregate([
-    {$unwind:"$friends"},
-    {$group:{_id:"$uid", sz:{$sum:1}}},
-    {$sort:-1}
+//friends dynamic
+//naive avg users per second
+//
+db.friends_dynamic.aggregate([
+    {$group:{
+        _id:{
+            month:{$month:"$processDate"},
+            dayOfMonth:{$dayOfMonth:"$processDate"},
+            hour:{$hour:"$processDate"},
+            minute:{$minute:"$processDate"},
+            second:{$second:"$processDate"}
+        },
+        count:{$sum:1}
+    }},
+    {$group:{
+        _id:null,
+        avgPerSecond:{$avg:"$count"}, 
+    }}
 ])
 
-//most friended
+
+//friends dynamic time series
+db.friends_dynamic.aggregate([
+    {$group:{
+        _id:{
+            month:{$month:"$processDate"},
+            dayOfMonth:{$dayOfMonth:"$processDate"},
+            hour:{$hour:"$processDate"},
+            minute:{$minute:"$processDate"},
+            second:{$second:"$processDate"}
+        },
+        count:{$sum:1}
+    }}
+],
+{allowDiskUse : true}
+)
+
+//most popular name
+db.friends_list.aggregate([
+    {$group:{_id:"$firstName", count:{$sum:1}}},
+    {$sort:{count:-1}},
+    {$limit:10}
+])
+
+//more then once added
+//aka ping-pong
+//i.e. it compares all added friends as Set and plain array 
+//v2 removed one unwind and one group
+db.friends_dynamic.aggregate([
+    {$project:{_id:1, uid:1, friendsAdded:1}},
+    {$unwind:"$friendsAdded"},
+    {$group:{_id:"$uid", addedSet:{$addToSet:"$friendsAdded"}, addedAll:{$push:"$friendsAdded"}}},
+    {$project:{_id:1, allsz:{$size:"$addedAll"}, setsz:{$size:"$addedAll"}}},
+    {$project:{_id:1, setsz:1, cmp:{$cmp:["$setsz", "$allsz"]}}},
+    {$match:{cmp:{$ne:0}}},
+    {$sort:{setsz:-1}}
+])
+
+//most friended with names
 db.friends_list.aggregate([
     {$unwind:"$friends"},
     {$group:{_id:{uid:"$uid", fn:"$firstName", ln:"$lastName"}, sz:{$sum:1}}},
@@ -29,7 +80,6 @@ db.friends_dynamic.aggregate([
     {$group:{_id:"$_id", count:{$sum:1}}},
     {$sort:{count:-1}}
 ])
-
 
 //count of added friends sorted by count desc
 db.friends_dynamic.aggregate([
--- TestDB/scripts.js
+++ TestDB/scripts.js
@@ -1,29 +1,3 @@
-//db.tasks.aggregate([{$match:{type:"friends_list"}},{$group:{_id:"$type", tot:{$push:{"data":"$data"}}}}])
-
-//get all uids from tasks
-{
-var redf = function(k, v) { 
-    var ret = []; 
-    for(var i in v) { 
-        ret = ret.concat(v[i].a) 
-    } 
-    return {a:ret};
-}
-
-var mapf = function() {
-    emit(this.type, {a: this.data})
-}
-
-db.tasks.mapReduce(mapf, redf, {query:{type:"friends_list"}, out:"tuid"})
-}
-
-//get all friends from one city from friends_raw as set
-db.friends_raw.aggregate([
-    {$unwind:"$friends"},
-    {$match:{"friends.city":2}},
-    {$group:{_id:null, friends:{$addToSet:"$friends.uid"}}}
-    ]).next().friends
-    
 function dropAll() {
     db.friends_list.drop();
     db.tasks.drop();
@@ -33,27 +7,14 @@ function dropAll() {
     db.friends_raw_tmp.drop();
 };
 
-function Init()
-{
-    db.first_man.insert({"uid":NumberLong(2763114)})
+function init() {
+    db.first_man.remove({});
+    db.first_man.insert({"uid":NumberLong(2763114)});
+    db.first_man.insert({start:new Date()});
+    db.friends_list.createIndex({uid:1});
 }
 
-db.friends_raw.insert({
-    uid: new NumberLong(1),
-    friends:[{uid:NumberLong(2), city:2},{uid:NumberLong(3),city:3},{uid:NumberLong(4),city:4}],
-    firstName:"fn",
-    lastName:"ln",
-    birthday: new Date(),
-    city: 33,
-    processDate: new Date()
-});
-
-db.friends_list.insert({
-    uid: new NumberLong(1),
-    friends:[NumberLong(1), NumberLong(2), NumberLong(3)],
-    firstName:"a",
-    lastName:"b",
-    birthday: new Date(),
-    city: 11,
-    processDate: new Date()
-});
+function past(m) {
+    var dt = new Date(); 
+    return new Date(dt.setMinutes(dt.getMinutes() - m));
+}
