5f62641786ce724996e10a8ee3a84f7cddca70dc
DePizzottri <depizzottri@gmail.com>
2015-08-24 19:29:58 +0800

    BD scripts

--- Database/scripts.js
+++ Database/scripts.js
@@ -1,11 +1,19 @@
 function dropAll() {
+    use vkcrawler_test_1
     db.queue.drop();
     db.friends.drop();
+    
+    use vkcrawler_persistence
+    db.persistent_journal.drop()
+    db.journal_index.drop()
+    db.persistent_snapshots.drop()
+    db.snaps_index.drop()
 };
 
 function init() {
+    use vkcrawler_test_1
     //db.tasks_frequency.insert({"type":"friends_list", "freq":NumberLong(3*60*60*1000)}); //initially every 3 hours
-    //2763114
+
     db.queue.insert({id:NumberLong(2763114)})
     db.queue.createIndex({"id":1})
     db.queue.createIndex({"lastUseDate":1})
