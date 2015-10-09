c60a2702c1849431bfe20298f2cc70bb3a4233b4
DePizzottri <depizzottri@gmail.com>
2015-10-09 16:07:10 +0800

    Config fixes

--- Database/scripts.js
+++ Database/scripts.js
@@ -14,6 +14,8 @@ function init() {
     use vkcrawler_test_1
     //db.tasks_frequency.insert({"type":"friends_list", "freq":NumberLong(3*60*60*1000)}); //initially every 3 hours
 
+    //2763114 - UU
+    //??? - IRK
     db.queue.insert({id:NumberLong(2763114)})
     db.queue.createIndex({"id":1})
     db.queue.createIndex({"friends_list.lastUseDate":1})
